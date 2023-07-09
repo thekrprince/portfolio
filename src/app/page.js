de2115daeebae34dcaac487d7d8a0098207117d0
princekr96 <princekr700@gmail.com>
2023-07-09 13:31:30 +0530

    updated navbar and summary

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-white dark:bg-zinc-900 max-w-2xl">
+    <main className="bg-white dark:bg-zinc-900 max-w-2xl mx-auto">
       <Summary />
     </main>
   );
--- src/components/summary.js
+++ src/components/summary.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 
 export default function Summary() {
   return (
-    <section className="flex justify-center">
-      <Image src="/pfp.png" alt="my-pic" width="64" height="64" />
-      <p className="self-end">
-        Hey, I am Kumar Prince (call me Prince). I am working in Walmart
-        currently and have so much experience.
+    <section className="flex flex-col items-center p-4">
+      <Image src="/pfp.png" alt="my-pic" width="84" height="84" />
+      <h2 className="m-2 sm:m-3 text-4xl font-bold">Hi, This is Prince </h2>
+      <p className="m-2">
+        A passionate Fullstack developer with expertise in MERN stack and a
+        knack for creating captivating Frontend experiences. Currently a
+        Software Engineer 3 at Walmart. When not coding, you can find him
+        strumming a guitar, reading books, writing tech blogs, and sharing
+        stuffs on Twitter.
       </p>
     </section>
   );

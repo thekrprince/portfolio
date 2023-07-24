0fb0c11c0c9cf4d86f9282fbbb47d25193f21505
princekr96 <princekr700@gmail.com>
2023-07-24 23:38:45 +0530

    added components for nav and updated the summary

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
-import { NavItemType, NavValues } from "@/types";
 import Link from "next/link";
 import { useState } from "react";
 import { NAV_ITEMS } from "@/constants";
+import About from "./about";
+import { NavValues } from "@/types";
+import Feed from "./feed";
+import Blog from "./blog";
+import Projects from "./projects";
 
 export default function NavBar() {
   const [navActiveClass, setNavActiveClass] = useState<string>(NAV_ITEMS[0]);
@@ -11,50 +15,30 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="navbar">
-      <ul className="my-3 flex justify-center items-center font-bold">
-        {NAV_ITEMS.map((navItem) => {
-          return (
-            <Link
-              href="/"
-              key={navItem}
-              onClick={() => navActiveHandler(navItem)}
-              className={`w-1/4 text-center navItem ${
-                navActiveClass === navItem &&
-                "underline underline-offset-8 decoration-4 decoration-pink-500"
-              }`}
-            >
-              <li>{navItem}</li>
-            </Link>
-          );
-        })}
-        {/* <li
-          className={`px-5 py-2 ${navActiveClass === NavValues.FEED && "bg-white"}`}
-          onClick={navActiveHandler}
-        >
-          <Link href="/">Feed</Link>
-        </li>
-        <li
-          className={`px-5 py-2 ${navActiveClass === NavValues.ABOUT && "bg-white"}`}
-          onClick={navActiveHandler}
-        >
-          <Link href="/about">About</Link>
-        </li>
-        <li
-          className={`px-5 py-2 ${navActiveClass === NavValues.BLOG && "bg-white"}`}
-          onClick={navActiveHandler}
-        >
-          <Link href="/blog">Blog</Link>
-        </li>
-        <li
-          className={`px-5 py-2 ${
-            navActiveClass === NavValues.PROJECTS && "bg-white"
-          }`}
-          onClick={navActiveHandler}
-        >
-          <Link href="/projects">Projects</Link>
-        </li> */}
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <ul className="my-3 flex justify-center items-center font-bold">
+          {NAV_ITEMS.map((navItem) => {
+            return (
+              <Link
+                href="/"
+                key={navItem}
+                onClick={() => navActiveHandler(navItem)}
+                className={`w-1/4 text-center navItem py-2 ${
+                  navActiveClass === navItem &&
+                  "underline underline-offset-8 decoration-4 decoration-pink-500"
+                }`}
+              >
+                <li>{navItem}</li>
+              </Link>
+            );
+          })}
+        </ul>
+      </nav>
+      {navActiveClass === NavValues.FEED && <Feed />}
+      {navActiveClass === NavValues.ABOUT && <About />}
+      {navActiveClass === NavValues.BLOG && <Blog />}
+      {navActiveClass === NavValues.PROJECTS && <Projects />}
+    </>
   );
 }
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -3,15 +3,21 @@ import Image from "next/image";
 export default function Summary() {
   return (
     <section className="flex flex-col items-center p-4 bg-stone-900 rounded-2xl">
-      <Image src="/pfp.png" alt="my-pic" width="84" height="84" />
-      <h2 className="m-2 sm:m-3 text-4xl font-bold">Hi, This is Prince </h2>
-      <p className="m-2">
-        A passionate Fullstack developer with expertise in MERN stack and a
-        knack for creating captivating Frontend experiences. Currently a
-        Software Engineer 3 at Walmart. When not coding, you can find him
-        strumming a guitar, reading books, writing tech blogs, and sharing
-        stuffs on Twitter.
-      </p>
+      <Image
+        src="/pfp.png"
+        alt="my-pic"
+        width="84"
+        height="84"
+        className="border-4 border-decoration-pink-500 rounded-full"
+      />
+      <h2 className="m-2 sm:m-3 text-4xl font-bold">Kumar Prince</h2>
+      <div className="m-2">
+        <p>Tinkers with Code!⚡</p>
+        <p className="mt-1">
+          Software Engineer 3 @ <span className="text-pink-500">Walmart</span>.
+          ♥️ Frontend. Tech Enthusiast. Public Speaker.
+        </p>
+      </div>
     </section>
   );
 }

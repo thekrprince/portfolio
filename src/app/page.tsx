037fbf3831ad20eb2867ac23e88f92b4b56cdfa2
princekr96 <princekr700@gmail.com>
2023-07-27 21:24:16 +0530

    added icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import WorkProfile from "@/components/work-profile";
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto">
+    <main className="max-w-2xl mx-3 md:mx-auto">
       <Summary />
       <NavBar />
     </main>
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Summary() {
   return (
-    <section className="flex flex-col items-center p-4 bg-stone-900 rounded-2xl">
+    <section className="flex flex-col p-8 bg-stone-900 rounded-2xl">
       <Image
         src="/pfp.png"
         alt="my-pic"
@@ -15,8 +15,79 @@ export default function Summary() {
         <p>Tinkers with Code!⚡</p>
         <p className="mt-1">
           Software Engineer 3 @ <span className="text-pink-500">Walmart</span>.
-          ♥️ Frontend. Tech Enthusiast. Public Speaker.
+          ♥️ Frontend. Content Creator. Public Speaker. Technical Blogger. Tech
+          Enthusiast.
         </p>
+      </div>
+      <div className="mx-2 mt-2 flex flex-wrap items-center gap-1 text-sm lg:text-base">
+        <span className="flex mr-2 md:mr-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z"
+            />
+          </svg>
+          <span className="ml-1">Available</span>
+        </span>
+        <span className="flex mr-2 md:mr-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+            />
+          </svg>
+          <span className="ml-1">/links</span>
+        </span>
+        <span className="flex mr-2 md:mr-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.87c1.355 0 2.697.055 4.024.165C17.155 8.51 18 9.473 18 10.608v2.513m-3-4.87v-1.5m-6 1.5v-1.5m12 9.75l-1.5.75a3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0 3.354 3.354 0 00-3 0 3.354 3.354 0 01-3 0L3 16.5m15-3.38a48.474 48.474 0 00-6-.37c-2.032 0-4.034.125-6 .37m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.17c0 .62-.504 1.124-1.125 1.124H4.125A1.125 1.125 0 013 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 016 13.12M12.265 3.11a.375.375 0 11-.53 0L12 2.845l.265.265zm-3 0a.375.375 0 11-.53 0L9 2.845l.265.265zm6 0a.375.375 0 11-.53 0L15 2.845l.265.265z"
+            />
+          </svg>
+          <span className="ml-1">June 9th</span>
+        </span>
+        <span className="flex mr-2 md:mr-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
+            />
+          </svg>
+          <span className="ml-1">Joined Jul 2023</span>
+        </span>
       </div>
     </section>
   );

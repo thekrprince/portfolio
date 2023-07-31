ff63c9f4f7dca64bcab09659a0ea29cb513f7a79
princekr96 <princekr700@gmail.com>
2023-07-31 21:39:26 +0530

    added blogs

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,16 +1,31 @@
+import { blogs } from "../../data/blogs";
+
 export default function Content() {
   return (
     <div className="mt-8">
       <p className="mb-8">
-        As much as I love to learn new technologies and read about it. Have
-        created some blogs and given some talks also to share the things which
-        have helped me.
+        As much as I like to learn new technologies and read about it. Love to
+        share the knowledge as well through my content.
       </p>
       <section className="mb-8">
         <h2 className="text-2xl font-bold">Talks</h2>
       </section>
       <section className="mb-8">
         <h2 className="text-2xl font-bold">Blogs</h2>
+        {blogs.map((blog, idx) => {
+          return (
+            <div key={idx} className="my-2">
+              <a
+                href={blog.link}
+                target="_blank"
+                className="text-pink-600 blog-links"
+              >
+                {blog.title}
+              </a>
+              <p className="opacity-75">{blog.date}</p>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
---
+++ src/data/blogs.js
@@ -0,0 +1,38 @@
+export const blogs = [
+  {
+    title: "Getting started with Monorepo using NX",
+    desc: "Blog to help developers to get started with Monorepo.",
+    link: "https://blog.thekrprince.com/monorepo-starter/",
+    date: "May 2023",
+  },
+  {
+    title: "Updating favicon in Fullstack(React + Django) Application",
+    desc: "A guide on how you can make favicon visible on browser while deploying fullstack application on Cloud server.",
+    link: "https://blog.thekrprince.com/favicon-in-fullstack/",
+    date: "Dec 2021",
+  },
+  {
+    title: "Local Storage, Session Storage and Cookies",
+    desc: "A short explanation on HTML5 Web Storage and Cookies.",
+    link: "https://blog.thekrprince.com/local-session-cookies/",
+    date: "Sep 2021",
+  },
+  {
+    title: "React in a nutshell",
+    desc: "A guide for beginners on how to learn ReactJS.",
+    link: "https://blog.thekrprince.com/react-journey/",
+    date: "Aug 2021",
+  },
+  {
+    title: "Git for beginners",
+    desc: "Basic Git commands every developer should know...",
+    link: "https://blog.thekrprince.com/git-for-beginners/",
+    date: "Jun 2021",
+  },
+  {
+    title: "Getting started with Monorepo using NX",
+    desc: "Difference b/w Spread operator, Rest operator and Destructuring in JavaScript.",
+    link: "https://blog.thekrprince.com/spread-rest-operator/",
+    date: "May 2021",
+  },
+];

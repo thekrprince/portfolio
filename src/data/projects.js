881dfb6fb3d741f5eed7ceec3fe9d58df1bce936
princekr96 <princekr700@gmail.com>
2023-07-31 16:33:15 +0530

    added a project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,24 +3,32 @@ export const projectsData = [
     title: "Dynamic Square",
     description:
       "Enter a number to generate NXN matrix with random background color. Developed it using React using Remix framework and Vanilla CSS",
-    image_url: "/images/dynamic_square.png",
+    image_url: "/images/projects/dynamic_square.png",
     github_url: "https://github.com/thekrprince/dynamic-square",
     demo_link: "https://nxnmatrix.netlify.app/",
+  },
+  {
+    title: "Random Trivia Quiz",
+    description:
+      "Built this Quiz app using React, TypeScript, Styled Components and https://opentdb.com/api.php api.",
+    image_url: "/images/projects/random-trivia-quiz.png",
+    github_url: "https://github.com/thekrprince/react-quiz-app",
+    demo_link: "https://test-your-knowledge-quiz.netlify.app/",
+  },
+  {
+    title: "Coin Toss Game",
+    description:
+      "A basic coin toss game created using HTML, CSS and Vanilla JavaScript. Deployed on GitHub pages",
+    image_url: "/images/projects/coin-toss-game.png",
+    github_url: "https://github.com/thekrprince/coin-toss-game",
+    demo_link: "https://thekrprince.github.io/coin-toss-game/",
   },
   {
     title: "Task Tracker",
     description:
       "To-do list app with drag and drop functionality. Developed using React, SCSS, Django and PostgreSQL",
-    image_url: "/images/dynamic_square.png",
-    github_url: "https://github.com/thekrprince/dynamic-square",
-    demo_link: "https://nxnmatrix.netlify.app/",
-  },
-  {
-    title: "Dynamic Square",
-    description:
-      "Enter a number to generate nxn matrix with random backgrounds color",
-    image_url: "/images/dynamic_square.png",
-    github_url: "https://github.com/thekrprince/dynamic-square",
+    image_url: "/images/projects/dynamic_square.png",
+    github_url: "https://github.com/PayalSasmal10/ToDo-FullStack",
     demo_link: "https://nxnmatrix.netlify.app/",
   },
 ];
